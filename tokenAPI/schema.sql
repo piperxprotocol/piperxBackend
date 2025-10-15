04dce2e62167d7a62106aee0458b0aeacd7efa0f
wanyi <xiangjiarui1111@gmail.com>
2025-10-15 09:27:47 +0800

    add swap table fields and create new volume tables

--- tokenAPI/schema.sql
+++ tokenAPI/schema.sql
@@ -2,12 +2,15 @@ CREATE TABLE swaps (
   id TEXT PRIMARY KEY,           
   vid INTEGER NOT NULL,          
   timestamp INTEGER NOT NULL,    
-  pair TEXT NOT NULL,            
+  pair TEXT NOT NULL,     
+  token0 TEXT NOT NULL,            
+  token1 TEXT NOT NULL,               
   token_0_amount TEXT NOT NULL,  
   token_1_amount TEXT NOT NULL,  
   account TEXT NOT NULL,         
   amount_usd TEXT,               
-  amount_native TEXT             
+  amount_native TEXT,
+  source TEXT      
 );
 
 CREATE TABLE tokens (
@@ -24,4 +27,14 @@ CREATE TABLE prices (
   ts INTEGER NOT NULL,          
   price_usd REAL NOT NULL,
   PRIMARY KEY (token_id, hour_bucket)
+);
+
+CREATE TABLE volume (
+  token_id TEXT NOT NULL,          
+  pool TEXT NOT NULL,              
+  source TEXT NOT NULL,            
+  hour_bucket INTEGER NOT NULL,   
+  volume_usd REAL DEFAULT 0,       
+  volume_native REAL DEFAULT 0,   
+  PRIMARY KEY (token_id, pool, source, hour_bucket)
 );
